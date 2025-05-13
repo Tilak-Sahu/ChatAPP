651abfb4e3fb5ee02e36e38968c5bffadab27c19
Tilak_Sahu <111271608+Tilak-Sahu@users.noreply.github.com>
2025-05-13 21:14:44 +0530

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    // config
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
